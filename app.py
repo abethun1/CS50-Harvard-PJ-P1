e2284c3c48883d28488a7fb1837b23e1278217c1
Antonio Bethune <antoniobethune@Antonios-MacBook-Pro.local>
2020-05-20 16:24:37 -0400

    added bootstrap

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
 from passlib.hash import pbkdf2_sha256
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
+from flask_bootstrap import Bootstrap
 
 from wtform_fields import *
 from models import *
@@ -12,6 +13,7 @@ app.secret_key = 'complicated_key'
 #Configure database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nzgmhxdlwwdcec:efb4d0febc3f1a2838ecf81355174183828b4281cd91c6239f4ffc79d54d4dda@ec2-52-71-231-180.compute-1.amazonaws.com:5432/d70tk20n2eq526'
 db = SQLAlchemy(app)
+Bootstrap(app)
 
 #Utilize flask login
 login = LoginManager(app)
@@ -22,8 +24,14 @@ def load_user(id):
 	return Users.query.get(int(id))
 
 #Signup route
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def index():
+	return "This will be a home page for people who arent logged in"	
+#	return render_template("index.html", form=reg_form)
+
+#Signup route
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
 	if current_user.is_authenticated:
 		return "You must log out to sign up"
 
@@ -41,7 +49,7 @@ def index():
 		db.session.commit()
 		return redirect(url_for('login'))
 
-	return render_template("index.html", form=reg_form)
+	return render_template("signup.html", form=reg_form)
 
 
 #Login route
